08d3552704ace8ff1ed65739fe13f190e13e2bbd
t0kage <60316226+t0kage@users.noreply.github.com>
2020-01-26 14:15:32 +0100

    Add files via upload

--- parser.py
+++ parser.py
@@ -3,10 +3,15 @@ import urllib3
 
 #connecting to website
 http = urllib3.PoolManager()
-url = 'http://anne-langner.de/feg-verwaltung.de/netzlaufwerk/VPlanWebsite/Monitor/index_schueler.htm'
+url1 = 'http://anne-langner.de/feg-verwaltung.de/netzlaufwerk/VPlanWebsite/Monitor/Schueler_Tag1/f1/subst_001.htm'
+url2 = 'http://anne-langner.de/feg-verwaltung.de/netzlaufwerk/VPlanWebsite/Monitor/Schueler_Tag1/f1/subst_001.htm'
 #logs in
 headers = urllib3.util.make_headers(basic_auth='feg:vertretung')
-response = http.request('GET', url, headers=headers)
-soup = BeautifulSoup(response.data)
+response1 = http.request('GET', url1, headers=headers)
+response2 = http.request('GET', url2, headers=headers)
+tag1 = BeautifulSoup(response1.data)
+tag2 = BeautifulSoup(response2.data)
 #soup = inhalt (anscheinend)
-print(soup)
+
+for element in tag1.find_all("tr"):
+    print(element.text)
