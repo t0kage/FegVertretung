0e4365c0acf8f7e45b218cae0fd80c1ddd962b5b
nfvfire <60316236+nfvfire@users.noreply.github.com>
2020-01-28 19:30:06 +0100

    Add files via upload

--- parser.py
+++ parser.py
@@ -1,19 +1,33 @@
 from bs4 import BeautifulSoup
 import urllib3
 
+
 #connecting to website
 http = urllib3.PoolManager()
-url1 = 'http://anne-langner.de/feg-verwaltung.de/netzlaufwerk/VPlanWebsite/Monitor/Schueler_Tag1/f1/subst_001.htm'
-url2 = 'http://anne-langner.de/feg-verwaltung.de/netzlaufwerk/VPlanWebsite/Monitor/Schueler_Tag2/f1/subst_001.htm'
 #logs in
 headers = urllib3.util.make_headers(basic_auth='feg:vertretung')
-response1 = http.request('GET', url1, headers=headers)
-response2 = http.request('GET', url2, headers=headers)
-tag1 = BeautifulSoup(response1.data)
-tag2 = BeautifulSoup(response2.data)
-#soup = inhalt (anscheinend)
+#funktion zur ermittlung der seitenanzahltag1
+def seitenanzahl(b):
+    for element in b.find_all("div"):
+        seite = element.text
+    seite = seite[-2]
+    if seite == ' ':
+        return(0)
+    return(seite)
+
+def urlgen(tag,seite):
+    tag = str(tag); seite = str(seite)
+    if len(seite) >= 2: 
+        seite = seite[0]
+    stamm = 'http://anne-langner.de/feg-verwaltung.de/netzlaufwerk/VPlanWebsite/Monitor/Schueler_Tag'+ tag+ '/f1/subst_00'+ seite+ '.htm'
+    stamm = str(stamm)
+    response1 = http.request('GET', stamm, headers=headers)
+    returnurl = BeautifulSoup(response1.data)
+    return(returnurl)
 
 def ausleser(url):
+
+
     rows = []
 
     i = 0
@@ -33,8 +47,38 @@ def ausleser(url):
                 z = 0
     return(rows)
 
-print(ausleser(tag2))
+
+url = urlgen(1,1)
+
+smax = seitenanzahl(url)
+#liste mit allem
+all1 = []
+all2 = []
+def zusammenfuegen(bigmama,tag):
+        for i in range(1,int(smax)+1):
+            url = urlgen(tag,i)
+            bigmama.extend(ausleser(url))
+        return(bigmama)
+
+all1, all2 = zusammenfuegen(all1, 1), zusammenfuegen(all2, 2)
+# all1/2 sind jetzt 3fach verschachtelte listen
+
+'''
+#url1 = 'http://anne-langner.de/feg-verwaltung.de/netzlaufwerk/VPlanWebsite/Monitor/Schueler_Tag1/f1/subst_001.htm'
+#url2 = 'http://anne-langner.de/feg-verwaltung.de/netzlaufwerk/VPlanWebsite/Monitor/Schueler_Tag2/f1/subst_001.htm'
+#soup = inhalt (anscheinend)
+
+#tag = tag2 #veränderbar
+
+#definiert Funktion zum auslesen der Daten in eine Liste aus Acht Listen
 
 
+#Es wird nach der Klasse gefragt und dann die betreffende Spalte ausgegeben
+
+klasse = input('Klasse(05A):')
+for l in range(12):
+    if klasse in str(ausleser(tag)[l][0]):
+        print(ausleser(tag)[l])
+'''
 
 
